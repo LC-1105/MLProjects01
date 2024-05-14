eba41ee28138e585af55e378635d5a206c4c6e01
CHAUHAN <lalit.a.chauhan@accenture.com>
2024-05-15 00:07:18 +0530

    prediction pipeline

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
-from src.mlproject.logger import logging
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
 
-from src.mlproject.exception import CustomException
-import sys
+from sklearn.preprocessing import StandardScaler
+from src.mlproject.pipelines.prediction_pipeline import CustomData,PredictPipeline
+application =Flask(__name__)
+app=application
 
-from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_ingestion import DataIngestionConfig
+## Route for homepage
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-from src.mlproject.components.data_transformation import DataTransformation,DataTransformationConfig 
-
-from src.mlproject.components.model_trainer import ModelTrainer,ModelTrainerConfig
-
-if __name__=="__main__":
-    logging.info("the execution has started")
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+        
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("After Prediction")
+        return render_template('home.html',results=results[0])
     
-    try:
-        #a=1/0
-        #data_ingestion_congig=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
-        #Data Transformation
-        data_transformation =DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-        
-        #Model Training
-        model_trainer=ModelTrainer()
-        r2,modelname=model_trainer.initiate_model_trainer(train_arr,test_arr)
-        print(r2, " ",modelname)
-        
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True) #Set this to ``'0.0.0.0'`` to Runs the application on a local development server.
+    
